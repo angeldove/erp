81742e705871d99187e8c67d0548e6321d73b886
angeldove <dev.roohiverma@gmail.com>
2016-05-09 21:01:10 +0530

    adding templates and home page view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def home(request):	
+    return render_to_response('index.html')
